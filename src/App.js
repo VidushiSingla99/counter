41a3775a7cf45ee549912394e1e9b6bb1886e9c7
VidushiSingla99 <vidushisingla99@gmail.com>
2022-05-08 11:33:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+const [counter, setCounter]=useState(0);
+const increaseCounter=()=>{
+setCounter( c=>c+1);
+}
+const decreaseCounter=()=>{
+  setCounter(c=>c-1);
+  }
+
+const resetCounter=()=>{
+  setCounter(c=>c=0);
 }
 
-export default App;
+  return (
+    <div>
+      <div className="d-flex justify-content-center mt-5">
+      <img src="https://cdn.pixabay.com/photo/2014/04/02/10/55/plus-304947_1280.png" className="w-10 mx-5" alt="plus-sign" onClick={increaseCounter}></img>
+    <img src="https://cdn4.iconfinder.com/data/icons/mathematical-symbols/43/Minus_Sign-512.png"  className="w-10 mx-5" alt="minus-sign" onClick={decreaseCounter}></img>
+      </div>
+      <h1 className="d-flex justify-content-center">
+        {counter}
+      </h1>
+      <div className="d-flex justify-content-center">
+      <button className="btn btn-danger mt-5" onClick={resetCounter}>RESET</button>
+      </div>
+
+    </div>
+  )
+}
